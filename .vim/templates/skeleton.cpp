8fe4d27f91e93f29118f5d6f289d0e11ad09cd01
SergioAlmeidaCiprianoJr <36544528+SergioAlmeidaCiprianoJr@users.noreply.github.com>
2020-01-16 08:28:13 -0300

    Update skeleton.cpp

--- .vim/templates/skeleton.cpp
+++ .vim/templates/skeleton.cpp
@@ -1,25 +1,22 @@
 #include <bits/stdc++.h>
- 
-#define ll long long
-#define prime 1000000000+7
 #ifndef ONLINE_JUDGE
 #define W(x) cerr << "\033[31m" << #x << " = " << x << "\033[0m" << "\n";
 #else
 #define W(x) 42;
 #endif
 
+#define ll long long
 using namespace std;
- 
-int n, m;
-int v[1000100];
+const ll prime { 1000000000+7 }, max { 1000100 };
  
 int main(){
 	
-	//ios::sync_with_stdio(false);
+	ios::sync_with_stdio(false);
+	int n;
+	scanf("%d", &n);
 
-	scanf("%d %d", &n, &m);
-
-	for(int i = 0; i < n; ++i){
+	for(int i = 0; i < n; ++i)
+	{
 
 	}
 
